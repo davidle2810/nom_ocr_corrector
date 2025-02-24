4c62dacf3d5f5072e9ce5e3d42e55fd73c3e174c
davidle2810 <anhlq28102002@gmail.com>
2025-02-24 20:06:00 +0700

    u

--- core/extract_bitext.py
+++ core/extract_bitext.py
@@ -220,12 +220,19 @@ def get_content_from_bitext(file_path):
     base_file_name = os.path.splitext(os.path.basename(file_path))[0] 
     for page_number in range(len(image_paths)):
         page_content = extract_page_content(os.path.join('images', f"{base_file_name}_page{page_number+1:03}.png"))
-        if page_content is None or langdetect.detect(page_content)!='vi':
+        try:
+            if langdetect.detect(page_content)!='vi':
+                shutil.copy(os.path.join('images', f"{base_file_name}_page{page_number+1:03}.png"), os.path.join(os.environ['OUTPUT_FOLDER'],'images_label', f"{base_file_name}_page{page_number+1:03}.png"))
+                sn_page_content = sn.extract_pages(os.path.join('images', f"{base_file_name}_page{page_number+1:03}.png"))
+                sn_content.append({'page_number': sn_page_number, 'file_page_number': page_number+1, 'content': sn_page_content})
+                sn_page_number = sn_page_number + 1
+            else:
+                vn_content.append({'page_number': vn_page_number, 'content': clean_text(page_content)})
+                vn_page_number = vn_page_number + 1
+        except:
             shutil.copy(os.path.join('images', f"{base_file_name}_page{page_number+1:03}.png"), os.path.join(os.environ['OUTPUT_FOLDER'],'images_label', f"{base_file_name}_page{page_number+1:03}.png"))
             sn_page_content = sn.extract_pages(os.path.join('images', f"{base_file_name}_page{page_number+1:03}.png"))
             sn_content.append({'page_number': sn_page_number, 'file_page_number': page_number+1, 'content': sn_page_content})
             sn_page_number = sn_page_number + 1
-        else:
-            vn_content.append({'page_number': vn_page_number, 'content': clean_text(page_content)})
-            vn_page_number = vn_page_number + 1
+            
     return sn_content, vn_content
